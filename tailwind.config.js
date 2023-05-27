79813af9c959becf410e4eaa7584a6db087c7afa
sasaki <takomannto1996@gmail.com>
2023-05-28 02:12:13 +0900

    tailwind.configにカラーコードを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,31 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        gray: {
+          default: "#535353",
+          50: "#F8F8F8",
+          100: "#DBDBDB",
+          200: "#ACACAC",
+          300: "#7B7B7B",
+          400: "#535353",
+          500: "#3C3C3C",
+        },
+        black: "#000",
+        blue : {
+          500: "#426BFF"
+        },
+        yellow: {
+          50: "#FFFFF5",
+          500: "#D5CC02"
+        },
+        orange: {
+          500: "#FF6F42"
+        },
+        red: {
+          500: "#DF0000"
+        }
+      }
     },
   },
   plugins: [],
