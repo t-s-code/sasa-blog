0e09fe7822e46a1f2558376fc1a4086bfa94d9aa
sasaki <takomannto1996@gmail.com>
2023-06-02 00:31:49 +0900

    トップページにAboutを追加

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,6 +6,9 @@ const Layout = ({ children }: PropsWithChildren) => {
     <div className='font-gothic'>
       <Navbar />
       {children}
+      <div className='w-full border-t h-12 border-gray-100 flex justify-center items-center'>
+        <p className='text-gray-300'>©2023 Tatsuya Sasaki</p>
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,22 @@
+import GitHubLogo from '@/components/Icons/GitHubLog'
+import Logo from '@/components/Icons/Logo'
+import TwitterLogo from '@/components/Icons/TwitterLogo'
 import SinglePost from '@/components/Post/SinglePost'
 import Tag from '@/components/Tag/Tag'
 import { NUMBER_OF_POSTS_PER_PAGE } from '@/constants/constants'
 import { MyPost, TagType, getAllTags, getPostsForTopPage } from '@/lib/notionAPI'
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
 
 interface HomeProps {
   fourPosts: MyPost[]
   allTags: TagType[]
 }
+
+const IMAGE_PATH = '/me.jpg'
+const GITHUB_URL = 'https://github.com/t-s-code'
 
 export default function Home({ fourPosts, allTags }: HomeProps) {
   return (
@@ -20,34 +27,71 @@ export default function Home({ fourPosts, allTags }: HomeProps) {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      {/* <main className='w-full px-56 mt-8 bg-gray-500'>
-        about
-      </main> */}
-
       <main className='w-full px-56 mt-8'>
-        <h1 className='text-5xl font-medium text-gray-500'>
-          Articles
-          <div className='w-96 mt-3 border-b-2' />
-        </h1>
-        {fourPosts.map((post, i) => (
-          <div key={i} className='mt-14 min-w-min'>
-            <SinglePost
-              title={post.title}
-              description={post.description}
-              date={post.date}
-              tags={post.tags}
-              slug={post.slug}
-              genre={post.genre}
-              thumbnail={post.thumbnail}
-            />
+        <section>
+          <h1 className='text-5xl font-medium text-gray-500'>
+            About
+            <div className='w-96 mt-3 border-b-2' />
+          </h1>
+          <div className='flex items-center justify-center h-80 mt-32 gap-28'>
+            <div className='relative w-72 h-72 rounded-full overflow-hidden'>
+              <Image src={IMAGE_PATH} alt='portrait' fill sizes='100vw' className='object-cover' />
+            </div>
+            <div className='h-72 w-[480px] flex flex-col justify-between items-start'>
+              <div>
+                <h2 className='text-4xl text-gray-400'>Front-End React(Next.js)</h2>
+                <h2 className='text-4xl text-gray-400 mt-3'>Developer</h2>
+              </div>
+              <div className='text-gray-400'>
+                <p>
+                  ポートフォリオをご覧いただきありがとうございます。都内でフロントエンドエンジニアをしています。
+                </p>
+                <p className=' mt-2'>名前: Tatsuya Sasaki</p>
+                <p>年齢: 26</p>
+                <p>場所: 東京</p>
+              </div>
+              <div className='flex items-center justify-start gap-3'>
+                <Link href={GITHUB_URL} target='_blank'>
+                  <GitHubLogo />
+                </Link>
+                <TwitterLogo />
+              </div>
+            </div>
           </div>
-        ))}
-        <div className='mt-12 mb-6 w-full flex justify-end'>
-          <Link href='posts/pages/1' className='text-gray-500 hover:opacity-80 hover:underline'>
-            ...もっと見る
-          </Link>
-        </div>
-        <Tag tags={allTags} />
+          {/* <div className='flex items-center justify-center h-60'>
+            <p>Skill Stack</p>
+            <p className='ml-8'>|</p>
+            <div>
+
+            </div>
+          </div> */}
+        </section>
+
+        <section className='mt-20'>
+          <h1 className='text-5xl font-medium text-gray-500'>
+            Articles
+            <div className='w-96 mt-3 border-b-2' />
+          </h1>
+          {fourPosts.map((post, i) => (
+            <div key={i} className='mt-14 min-w-min'>
+              <SinglePost
+                title={post.title}
+                description={post.description}
+                date={post.date}
+                tags={post.tags}
+                slug={post.slug}
+                genre={post.genre}
+                thumbnail={post.thumbnail}
+              />
+            </div>
+          ))}
+          <div className='mt-12 mb-6 w-full flex justify-end'>
+            <Link href='posts/pages/1' className='text-gray-500 hover:opacity-80 hover:underline'>
+              ...もっと見る
+            </Link>
+          </div>
+          {/* <Tag tags={allTags} /> */}
+        </section>
       </main>
     </div>
   )
